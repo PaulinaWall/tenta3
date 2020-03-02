28ee71d51c4b7786d5134268a05d104790dcf40e
Paulina Arnesson <paulinaarnesson@hotmail.com>
2020-03-02 10:24:17 +0100

    got response from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import SearchCity from './components/SearchCity';
 import WeatherReport from './components/WeatherReport';
+import axios from 'axios';
 
 class App extends React.Component {
 	state = {
 		errorMessage: false,
 		report: null,
+	}
+
+	getAPI() {
+		axios.get('http://api.openweathermap.org/data/2.5/weather?q=Lund&appid=a9f6719e37f20890ebff5d91724dec1f')
+		.then(response => {
+			console.log(response.data)
+		})
+		.catch(err => {
+			console.error(err)
+		})
+	}
+
+	componentDidMount() {
+		this.getAPI()
 	}
 
 	render() {
